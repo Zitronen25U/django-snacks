6b272db162e7bf5a0e85c32f9f01e7a4462952e6
Brian Lemons <brian_lemons@hotmail.com>
2021-06-21 16:07:44 -0700

    DONE

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django import urls
+from django.conf.urls import url
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# Create your tests here
+
+class snacks_test(SimpleTestCase):
+    def test_home(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_html(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_html(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateUsed(response, 'base.html')
